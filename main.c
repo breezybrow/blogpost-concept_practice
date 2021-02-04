7ce8ce3239d90b59004bbe2ba2fc4c8d7b2a575a
breezybrow <browder.bree@gmail.com>
2021-02-04 05:29:47 +0000

    Compiling exercise

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
-* main- printing a quote
+* main- printing a quote to stdout
 * Return: always zero
 */
 
 int main(void)
 {
-    printf("Hello, World\n");
+	printf("Hello, World\n");
 
-    return(0);
+	return (0);
 }
